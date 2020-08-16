83322a4d403dccf24f2aaf82dd65477c145a990f
Nazar Ashirov <newnewqwes@gmail.com>
2020-08-16 22:45:33 +0300

    ыв

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,21 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import TodoList from "./components/TodoList";
 
+export interface ITodo {
+  title: string;
+  id: number;
+  completed: boolean;
+}
+
 const App: React.FC = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
   const hendlerAdd = (value: string) => {
-    console.log(value);
+    const newTodo: ITodo = {
+      title: value,
+      id: Date.now(),
+      completed: false,
+    };
+    setTodos((prev) => [newTodo, ...prev]);
   };
   return (
     <>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { ITodo } from "../App";
 interface ITodos {
-  todos: any[];
+  todos: ITodo[];
 }
 const TodoList: React.FC<ITodos> = ({ todos }) => {
   return (
     <ul>
       {todos.map((n) => {
         return (
-          <li>
+          <li key={n.id}>
             <label>
               <input type="checkbox" />
-              <span>n</span>
+              <span>{n.title}</span>
               <i className="material-icons red-text">delete</i>
             </label>
           </li>
